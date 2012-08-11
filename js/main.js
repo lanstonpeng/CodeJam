4caa50e772346b05463c960c0926f9e12d458d0c
lanstonpeng <lanstonpeng@gmail.com>
2012-08-11 11:56:52 +0800

    notebook base

--- js/main.js
+++ js/main.js
@@ -24,17 +24,60 @@ var Notebook = (function(){
 	//load
 	//remainder
 
+//database stucture
+/*
+{
+	gender:[{
+		title:"title",
+		content:"content",
+		endTime:""
+	}]
+}
+*/
 	var 
-		_init = function(){
+		_defaultReminderCallback = function(gender){
+			
+			someObj[gender].talk();
+		},
+		_gender,
+		_init = function( gender){
 			//load the old notebook
+			//see if there's a reminder 
+			var gender = _gender = gender || "girl",
+				Data = store.get("notebook"),
+				notebooks = Data[gender],
+				now = (new Date()).getTime();
+
+			if(notebooks){
+				for(var i in notebooks){
+					i.endTime < now && this._setReminder(i.endTime);
+				}
+			}
+		},
+		_save = function(params){
+			var params = params || {},
+				title = params.title,
+				content =params.content,
+				endTime = params.endTime,
+				gender = params.gender || "girl",
+				Data = store.get("notebook") || {};
+
+			Data[gender].push({
+				title : title
+				content: content,
+				endTime : endTime
+			});
 
 		},
-		_save = function(){
+		_setReminder = function( endTime,callback ){
+			var endTime =parseInt(endTime || (new Date()).getTime(),10),
+				callback = callback || _defaultReminderCallback;
 
-
-		},
-		_setReminder = function( endTime ,callback){
-
+			setInterval(function(){
+				if((new Date()).getTime() >= endTime){
+					_defaultReminderCallback(_gender);
+				}
+			},1000);
 
 		};
 
@@ -42,7 +85,6 @@ var Notebook = (function(){
 		init : _init,
 		save : _save,
 		setReminder : _setReminder
-
 	}
 
 })();
